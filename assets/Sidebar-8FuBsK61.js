39ebc8ac8019b6de84827d3e8252496f56464771
Afsal7359 <afsal4771@gmail.com>
2025-05-09 17:46:56 +0530

    updated code

--- assets/Sidebar-8FuBsK61.js
+++ assets/Sidebar-8FuBsK61.js
@@ -1,4 +1,4 @@
-import{r as o,j as e,N as c}from"./index-B1yHLzvF.js";/**
+import{r as o,j as e,N as c}from"./index-CfzXU5K9.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
