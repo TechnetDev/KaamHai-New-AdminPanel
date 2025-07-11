ce525587b92e02f2e2891a84a4cc15675451687c
Afsal7359 <afsal4771@gmail.com>
2025-07-11 16:50:37 +0530

    update code

--- assets/Sidebar-DXbzc6iO.js
+++ assets/Sidebar-DXbzc6iO.js
@@ -1,4 +1,4 @@
-import{r as l,j as e,N as c}from"./index-BwMKDibX.js";/**
+import{r as l,j as e,N as c}from"./index-BOgnwTZV.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
